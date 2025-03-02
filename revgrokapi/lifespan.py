55f881f344d11915f69413961426c6d8b5f737d3
LLinkedlist <72634327+linkedlist771@users.noreply.github.com>
2025-03-02 19:08:34 +0800

    Update lifespan.py

--- revgrokapi/lifespan.py
+++ revgrokapi/lifespan.py
@@ -4,21 +4,22 @@ from fastapi import FastAPI
 from loguru import logger
 
 from revgrokapi.db import init_db
+from revgrokapi.periodic_checks.limit_sheduler import LimitScheduler
 from revgrokapi.utils.time_zone_utils import set_cn_time_zone
 
 # from rev_claude.client.client_manager import ClientManager
-# from rev_claude.periodic_checks.limit_sheduler import LimitScheduler
 
 
 async def on_startup():
     logger.info("Lifespan Starting up")
     set_cn_time_zone()
     await init_db()
+    await LimitScheduler.start()
 
 
 async def on_shutdown():
     logger.info("Lifespan Shutting down")
-    # await LimitScheduler.shutdown()
+    await LimitScheduler.shutdown()
 
 
 @asynccontextmanager

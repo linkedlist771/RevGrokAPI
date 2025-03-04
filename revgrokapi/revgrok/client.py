144d3bbba82ac55fa2042c6e20f653a47f84ed80
LLinkedlist <72634327+linkedlist771@users.noreply.github.com>
2025-03-04 13:26:17 +0800

    Update client.py

--- revgrokapi/revgrok/client.py
+++ revgrokapi/revgrok/client.py
@@ -166,8 +166,8 @@ class GrokClient:
         rate_limit_response = await self.client.post(
             url, headers=self.headers, json=payload
         )
-        json_response = rate_limit_response.json()
-        logger.debug(json_response)
+        json_response = rate_limit_response
+        logger.debug(rate_limit_response)
         return request_kind, json_response
 
     async def get_rate_limit(self):
